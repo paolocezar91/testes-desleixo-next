563ef28f07949b5427530e85ee606136867e01d0
Paolo <pcezar87@gmail.com>
2025-10-29 16:09:13 -0300

    Fixes for mobile

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,5 @@
 import { QuizModel } from "@/models/Quiz";
 import Image from "next/image";
-import { useState } from "react";
-import YouTube from "react-youtube";
-import LoadingSpinner from "./spinner";
 
 export default function Header({
   children,
@@ -11,16 +8,18 @@ export default function Header({
   children: React.ReactNode;
   quiz: QuizModel;
 }) {
-  const [loading, setLoading] = useState(true);
-
   return (
     <div className="text-center">
-      <h1 className="magedon text-8xl flex justify-center">
+      <h1 className="magedon text-7xl md:text-8xl flex justify-center">
         <span>Testes do desleix</span>
         <span className="relative rotate-y-180">O</span>
       </h1>
       <h2 className="text-3xl font-bold mb-4">{quiz.title}</h2>
-      <p className="mb-4">{quiz.description}</p>
+      <p className="mb-4">
+        <a href={quiz.coverVideo} target="_blank" className="underline">
+          {quiz.description}
+        </a>
+      </p>
 
       {quiz.coverImage && (
         <div className="relative w-full h-64 mb-4">
@@ -32,15 +31,6 @@ export default function Header({
           />
         </div>
       )}
-
-      {loading && <LoadingSpinner />}
-
-      {quiz.coverVideo && (
-        <div className="relative w-full mb-4 flex align-center justify-center">
-          <YouTube videoId="AXS9-Rjsq_w" onReady={() => setLoading(false)} />
-        </div>
-      )}
-
       {children}
     </div>
   );
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
       <Head>
         <title>{title}</title>
       </Head>
-      <main className="min-h-screen relative">
+      <main className="h-screen w-full md:min-h-screen relative">
         <div className="absolute right-5 top-5 z-1">
           <a
             className="rounded-full w-7 h-7 bg-white text-black text-center block border-2
@@ -26,10 +26,10 @@ export default function RootLayout({
             ?
           </a>
         </div>
-        <div className="absolute overflow-auto top-0 left-1/2 transform -translate-x-1/2 h-full mx-auto z-1">
-          <div className="w-250 p-8">{children}</div>
+        <div className="absolute md:overflow-auto md:top-0 md:left-1/2 md:transform md:-translate-x-1/2 md:h-full md:mx-auto z-1">
+          <div className="w-full md:w-250 p-8">{children}</div>
         </div>
-        <div className="absolute w-full h-200 bottom-0 z-0">
+        <div className="md:block absolute w-full h-100 md:h-200 bottom-0 z-0">
           <Image src="/fire.gif" fill alt="fire" />
         </div>
       </main>
