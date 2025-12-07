333fda7605e20e2ed9e2423836d79a0771993aa9
Paolo <pcezar87@gmail.com>
2025-12-07 18:45:02 -0300

    safety update vercel

--- src/components/progress.tsx
+++ src/components/progress.tsx
@@ -8,7 +8,7 @@ export default function ProgressBar({
   return (
     <div className="w-full my-4 bg-gray-200 rounded-full h-2.5">
       <div
-        className="bg-blue-600 h-2.5 rounded-full transition-all"
+        className="bg-red-600 h-2.5 rounded-full transition-all"
         style={{
           width: `${(currentQuestionIndex / questionLength) * 100}%`,
         }}
